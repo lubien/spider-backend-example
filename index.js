297ba6d81d643e9615beafd340edd4db4dbfa73c
lubien <lubien1996@gmail.com>
2018-03-20 10:56:58 -0300

    Exemplo variáveis no GET

--- index.js
+++ index.js
@@ -4,8 +4,31 @@ const app = new Koa()
 
 const router = new Router()
 
+const users = {
+    1: { username: 'lubien' },
+    2: { username: 'leochrisis' },
+}
+
 router.get('/', (ctx, next) => {
     ctx.body = 'Hello World'
+})
+
+router.get('/users', (ctx, next) => {
+    ctx.body = [
+        { username: 'lubien' },
+        { username: 'leochrisis' }
+    ]
+})
+
+router.get('/users/:id', (ctx, next) => {
+    const id = ctx.params.id
+    if (users[id]) {
+        ctx.body = users[ctx.params.id]
+    } else {
+        ctx.body = 'Not found'
+        ctx.status = 404
+    }
+    
 })
 
 router.get('/outro', (ctx, next) => {
