b9c9102fa51d0facb6c999f431d0017ae020ed78
lubien <lubien1996@gmail.com>
2018-03-20 10:50:10 -0300

    Inicializa o koa-router

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const Koa = require('koa')
+const Router = require('koa-router')
 const app = new Koa()
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
+const router = new Router()
+
+router.get('/', (ctx, next) => {
+    ctx.body = 'Hello World'
 })
+
+router.get('/outro', (ctx, next) => {
+    ctx.body = 'Estou em outro'
+})
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
 
 app.listen(3000)
